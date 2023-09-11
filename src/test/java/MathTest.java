d8bb08b8fd2df8369e5e852ac26e08fa3397cfff
Zoom <zoomfo@gmail.com>
2023-09-11 20:25:02 +0300

    AddGitIgnore

---
+++ src/test/java/MathTest.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class MathTest {
+
+    @Test
+    public void MathTest() {
+        str().equals("sss");
+    }
+
+    private String str() {
+        return "str";
+    }
+}
