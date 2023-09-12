95d8de74f4dfa7ad72bee7af1f58241e9a25fe5f
Zoom <zoomfo@gmail.com>
2023-09-12 15:45:18 +0300

    fix

--- src/main/java/comfy/BasePage.java
+++ src/main/java/comfy/BasePage.java
@@ -7,7 +7,7 @@ public class BasePage {
     public static final Logger LOGGER = Logger.getRootLogger();
 
     public void MathTest() {
-      ad x иsвcvstr().equals("sss");
+      ad иsвcvstr().equals("sss");
     }
 
     private String str() {
