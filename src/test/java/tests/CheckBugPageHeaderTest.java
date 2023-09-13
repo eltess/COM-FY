3d4d04799c5f429dcb41460f7a106d455fcd0628
Zoom <zoomfo@gmail.com>
2023-09-13 11:50:06 +0300

    AddDesiredCapabilities

--- src/test/java/tests/CheckBugPageHeaderTest.java
+++ src/test/java/tests/CheckBugPageHeaderTest.java
@@ -1,10 +1,11 @@
-package tests.bug;
+package tests;
 
-import tests.BaseTest;
+import org.testng.annotations.Test;
 
 public class CheckBugPageHeaderTest extends BaseTest {
 
+    @Test
     public void checkBugPageHeaderTest() {
-
+        System.out.println();
     }
 }
