71cadc123519858b545092665628ce5944729b0f
Kruk Ivan <ivan.kruk@freshcodeit.com>
2023-09-13 14:07:36 +0300

    Fixed PR for pmd checks, 3 pmd errors left

---
+++ src/main/java/comfy/ButtonHelper.java
@@ -0,0 +1,27 @@
+package comfy;
+
+import com.codeborne.selenide.SelenideElement;
+import comfy.util.UiConfiguration;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class ButtonHelper implements ElementAction {
+
+    private final String buttonName;
+    public SelenideElement elementName;
+
+    public ButtonHelper(String id, String name) {
+        this.elementName = $x("//*[@%s='%s']".formatted(UiConfiguration.PLATFORM.getIdAttribute(), id));
+        this.buttonName = "%s %s".formatted(name, "Button");
+    }
+
+    @Override
+    public String name() {
+        return buttonName;
+    }
+
+    @Override
+    public SelenideElement element() {
+        return elementName;
+    }
+}
--- src/main/java/comfy/util/DesiredCapabilities.java
+++ src/main/java/comfy/util/DesiredCapabilities.java
@@ -2,15 +2,12 @@ package comfy.util;
 
 import comfy.config.UIConfiguration;
 import io.appium.java_client.AppiumDriver;
-
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.ios.options.XCUITestOptions;
 import io.qameta.allure.Step;
+import lombok.SneakyThrows;
 import org.aeonbits.owner.ConfigFactory;
-
-import java.net.MalformedURLException;
 import java.net.URL;
-
 
 
 public class DesiredCapabilities {
@@ -25,7 +22,7 @@ public class DesiredCapabilities {
     }
 
     private AppiumDriver getLocalEmulatorDriver() {
-        return uiConfiguration.platformName().equals("android") ? getAndroidLocalDriver() : getIosLocalDriver();
+        return "android".equals(uiConfiguration.platformName()) ? getAndroidLocalDriver() : getIosLocalDriver();
     }
 
     private AppiumDriver getAndroidLocalDriver() {
@@ -42,13 +39,12 @@ public class DesiredCapabilities {
         return new AppiumDriver(getUrl(), options);
     }
 
+    @SneakyThrows
     private URL getUrl() {
         URL remoteUrl;
-        try {
+
             remoteUrl = new URL(url());
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
+
         return remoteUrl;
     }
 
