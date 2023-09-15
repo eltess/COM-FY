6150d555479a64495c53841f54a297036ee78ebc
Zoom <zoomfo@gmail.com>
2023-09-15 13:08:44 +0300

    AddElementUtil

---
+++ src/main/java/comfy/util/ElementUtil.java
@@ -0,0 +1,47 @@
+package comfy.util;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.byAttribute;
+import static com.codeborne.selenide.Selenide.*;
+import static comfy.enums.Platform.ANDROID;
+import static comfy.util.UiConfiguration.PLATFORM;
+import static io.appium.java_client.AppiumBy.accessibilityId;
+import static org.openqa.selenium.By.className;
+
+@SuppressWarnings("PMD")
+public class ElementUtil {
+
+    public static String getTextFrom(SelenideElement element) {
+        return switch(PLATFORM) {
+            case IOS -> element.text();
+            case ANDROID -> {
+                SelenideElement textElement = element.$(className(PLATFORM.getTextClass()));
+                yield textElement.exists() ? textElement.text() : "";
+            }
+        };
+    }
+
+    public static SelenideElement getElementWithId(String id) {
+        return $(PLATFORM.equals(ANDROID) ? byAttribute("resource-id", id)
+            : accessibilityId(id));
+    }
+
+    public static ElementsCollection getElementsWithId(String id) {
+        return $$(PLATFORM.equals(ANDROID) ? byAttribute("resource-id", id)
+            : accessibilityId(id));
+    }
+
+    public static SelenideElement getElementContainsId(String id) {
+        return $x("//*[contains(@%s,'%s')]".formatted(PLATFORM.getIdAttribute(), id));
+    }
+
+    public static ElementsCollection getElementsContainsId(String id) {
+        return $$x("//*[contains(@%s,'%s')]".formatted(PLATFORM.getIdAttribute(), id));
+    }
+
+    public static SelenideElement getElementWithText(String text) {
+        return $x("//*[@%s='%s']".formatted(PLATFORM.getTextAttribute(), text));
+    }
+}
